db0c21225da62f19b1a5d7f46a272fc1a390c090
ckmessi <ckmessi@163.com>
2016-01-19 17:00:56 +0800

    fine tune

---
+++ src/ObjectInfo.cpp
@@ -0,0 +1,39 @@
+﻿#include "ObjectInfo.h"
+
+
+vector<ObjectInfo> parseObjectInfoList(string str, char c)
+{
+	// list存的每个字符串是一个对象信息
+	vector<string> list;
+    int i = 0;
+    int lastPos = 0;
+	while(i < (int)str.length()){
+		if(str[i] == c){
+			string temp = str.substr(lastPos, i-lastPos);
+			list.push_back(temp);
+			lastPos = i+1;			
+		}
+		i++;
+    }
+    string temp = str.substr(lastPos);
+    list.push_back(temp);
+    
+    // objectList存的每个对象是一个对象
+    vector<ObjectInfo> objectList;
+    i = 0;
+    while(i < (int)list.size() - 4){
+		int x1 = atoi(list[i].c_str());
+		i++;
+		int y1 = atoi(list[i].c_str());
+		i++;
+		int x2 = atoi(list[i].c_str()); 
+		i++;
+		int y2 = atoi(list[i].c_str());
+		i++;
+		string title = list[i];
+		i++;
+		ObjectInfo oi(x1, y1, x2, y2, title);
+		objectList.push_back(oi);
+    } 
+    return objectList;	
+}
---
+++ src/ObjectInfo.h
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+
+class ObjectInfo{
+    
+public:
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    string category;
+	
+public: 
+    ObjectInfo(int x1, int y1, int x2, int y2, string category){
+		this->x1 = x1;
+		this->y1 = y1;
+		this->x2 = x2;
+		this->y2 = y2;
+		this->category = category;
+    }
+
+   
+    // 根据
+
+};
+
+// 根据字符串解析出检测目录
+vector<ObjectInfo> parseObjectInfoList(string objectInfoStr, char c);
